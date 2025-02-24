0c4fdc1f7239f9d1573228e38bf803bc074ae532
jayrajcomputere <39955018-jayrajcomputere@users.noreply.replit.com>
2025-02-24 08:57:45 +0000

    Agent query: Is the navigation bar now centered with the logo on the left and navigation links in the middle?
    Center the navigation bar in the website header
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a407f087-ee85-4013-a197-6e6d6ca14563/33f5923f-d4e0-4c03-bf17-6a84f14d0fef.jpg

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -4,19 +4,24 @@ export function Navbar() {
   return (
     <nav className="bg-primary">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <Link href="/">
-            <a className="text-white text-xl font-bold">TechEdu Institute</a>
-          </Link>
-          <div className="flex space-x-4">
+        <div className="flex items-center justify-center h-16 relative">
+          {/* Logo on the left */}
+          <div className="absolute left-4">
             <Link href="/">
-              <a className="text-white hover:text-yellow-300">Home</a>
+              <a className="text-white text-xl font-bold">TechEdu Institute</a>
+            </Link>
+          </div>
+
+          {/* Centered navigation links */}
+          <div className="flex space-x-8">
+            <Link href="/">
+              <a className="text-white hover:text-yellow-300 text-lg">Home</a>
             </Link>
             <Link href="/courses">
-              <a className="text-white hover:text-yellow-300">Courses</a>
+              <a className="text-white hover:text-yellow-300 text-lg">Courses</a>
             </Link>
             <Link href="/contact">
-              <a className="text-white hover:text-yellow-300">Contact</a>
+              <a className="text-white hover:text-yellow-300 text-lg">Contact</a>
             </Link>
           </div>
         </div>
